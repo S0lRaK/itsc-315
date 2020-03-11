20600efd3e209b1d40e355ce8896b70591ef39e3
Carlos J. Garcia Carmona <carlos.garciacarmona@edu.sait.ca>
2020-03-10 22:56:02 -0600

    Lab 7 Problem 2: DB credentials as environment variables

--- labs/lab07/ITSC315Lab7/src/java/sait/dataaccess/DBUtil.java
+++ labs/lab07/ITSC315Lab7/src/java/sait/dataaccess/DBUtil.java
@@ -2,13 +2,20 @@ package sait.dataaccess;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import javax.naming.Context;
+import javax.naming.InitialContext;
 
 public class DBUtil {
 
     public static Connection getConnection() throws Exception {
         Connection conn;
         Class.forName ("com.mysql.jdbc.Driver").newInstance ();
-        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/itsc315lab7", "itsc315", "password");
+        
+        Context env = (Context)new InitialContext().lookup("java:comp/env");
+        String username = (String)env.lookup("db-username");
+        String password = (String)env.lookup("db-password");
+        
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/itsc315lab7", username, password);
         return conn;
     }
 }
