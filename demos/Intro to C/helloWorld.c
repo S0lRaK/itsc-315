9f18188e8385b715fa1f2fc707266b9c3f7b9ef3
Carlos J. Garcia Carmona <carlos.garciacarmona@edu.sait.ca>
2020-03-23 09:51:25 -0600

    C hello world

---
+++ demos/Intro to C/helloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h> /* preprocessor directive */
+
+int main(int argc, char *argv[]) /* main function */
+{
+	printf("Hello world\n"); /* output to screen */
+	return 0; /* program result, returned to OS */
+}
